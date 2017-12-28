5e4505c7325d70403dd4329ddff1145370b79ec9
Andri Bremm <andri.bremm@senacor.com>
2017-12-28 23:31:33 +0100

    Task 2.2: Added a chat message service.

---
+++ services/src/main/java/com/senacor/code/fullstack/chat/service/ChannelNotFoundException.java
@@ -0,0 +1,7 @@
+package com.senacor.code.fullstack.chat.service;
+
+/**
+ * Exception to represent the case when a requested communication channel does not exist.
+ */
+public class ChannelNotFoundException extends Exception {
+}
