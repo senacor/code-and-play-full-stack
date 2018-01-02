5ea9fb5c3829fba9428a91cf3c76f849f6ef2351
Andri Bremm <andri.bremm@senacor.com>
2018-01-02 13:19:36 +0100

    Task 2.4: Integration test for ChannelController.

---
+++ services/src/test/java/com/senacor/code/fullstack/chat/rest/ChatMessageControllerIT.java
@@ -0,0 +1,49 @@
+package com.senacor.code.fullstack.chat.rest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChatMessageControllerIT {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    public void loadMessages() throws Exception {
+        String channel = "dev";
+        mockMvc.perform(get("/api/v1/{channel}/messages", channel)
+                .accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$[0].message").value("Hello"))
+                .andExpect(jsonPath("$[1].message").value("World!"));
+    }
+
+    @Test
+    public void loadMessagesForNotExistingChannel() throws Exception {
+        String channel = "not-a-channel";
+        mockMvc.perform(get("/api/v1/{channel}/messages", channel)
+                .accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .andExpect(status().isNotFound());
+    }
+}
