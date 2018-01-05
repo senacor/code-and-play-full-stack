808da3b32d904ed4585312100ece2d78d081bf21
Andri Bremm <andri.bremm@senacor.com>
2018-01-05 08:11:25 +0100

    Task 10.2 A POST endpoint to save new chat message.

--- services/src/test/java/com/senacor/code/fullstack/chat/rest/ChatMessageControllerIT.java
+++ services/src/test/java/com/senacor/code/fullstack/chat/rest/ChatMessageControllerIT.java
@@ -1,5 +1,7 @@
 package com.senacor.code.fullstack.chat.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.senacor.code.fullstack.chat.domain.ChatMessage;
 import com.senacor.code.fullstack.chat.domain.ChatMessage;
 import com.senacor.code.fullstack.chat.repository.ChatMessageRepository;
 import org.junit.Before;
@@ -14,6 +16,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.hamcrest.core.StringStartsWith.startsWith;
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
@@ -27,6 +32,9 @@ public class ChatMessageControllerIT {
 
     @Autowired
     private WebApplicationContext wac;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Before
     public void setup() {
@@ -53,5 +61,26 @@ public class ChatMessageControllerIT {
         mockMvc.perform(get("/api/v1/{channel}/messages", channel)
                 .accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void newMessages() throws Exception {
+        String channel = "dev";
+        ChatMessage newMessage = new ChatMessage(null, "xy@test.de", "My first message");
+
+        mockMvc.perform(post("/api/v1/{channel}/messages", channel)
+                .content(objectMapper.writeValueAsString(newMessage))
+                .contentType(MediaType.parseMediaType("application/json;charset=UTF-8"))
+                .accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("location", startsWith("/api/v1/dev/messages/")))
+                .andDo(result -> {
+                    // Check if the new message can be found in the database
+                    String location = result.getResponse().getHeader("location");
+                    String newMessageId = location.substring(location.lastIndexOf("/") + 1);
+                    ChatMessage savedMessage = repository.findById(newMessageId).get();
+                    assertEquals("xy@test.de", savedMessage.getSender());
+                    assertEquals("My first message", savedMessage.getMessage());
+                });
     }
 }
--- services/src/test/java/com/senacor/code/fullstack/chat/rest/ChatMessageControllerTest.java
+++ services/src/test/java/com/senacor/code/fullstack/chat/rest/ChatMessageControllerTest.java
@@ -4,12 +4,16 @@ import com.senacor.code.fullstack.chat.domain.ChatMessage;
 import com.senacor.code.fullstack.chat.service.ChannelNotFoundException;
 import com.senacor.code.fullstack.chat.service.ChatMessageService;
 import org.junit.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -31,4 +35,17 @@ public class ChatMessageControllerTest {
         verify(serviceMock).loadChatMessages("dev");
     }
 
+    @Test
+    public void newChatMessages() throws ChannelNotFoundException, URISyntaxException {
+        ChatMessage savedMessage = new ChatMessage("dev", "sender@test.de", "Hello World!");
+        savedMessage.setId("id-of-msg");
+        //when(serviceMock.createChatMessage("dev", "sender@test.de", "Hello Word!")).thenReturn(savedMessage);
+        when(serviceMock.createChatMessage(any(), any(), any())).thenReturn(savedMessage);
+
+        ResponseEntity<Void> result = controller.newChatMessages("dev", new ChatMessage(null, "sender@test.de", "Hello World!"));
+
+        assertEquals(HttpStatus.CREATED, result.getStatusCode());
+        assertEquals(new URI("/api/v1/dev/messages/id-of-msg") , result.getHeaders().getLocation());
+        verify(serviceMock).createChatMessage("dev", "sender@test.de", "Hello World!");
+    }
 }
