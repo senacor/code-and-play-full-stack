634f1cfe5ab9d1c002555b804537892ba44637c5
Andri Bremm <andri.bremm@senacor.com>
2018-01-05 08:11:25 +0100

    Task 10.2 A POST endpoint to save new chat message.

--- services/src/main/java/com/senacor/code/fullstack/chat/rest/ChatMessageController.java
+++ services/src/main/java/com/senacor/code/fullstack/chat/rest/ChatMessageController.java
@@ -5,10 +5,9 @@ import com.senacor.code.fullstack.chat.service.ChannelNotFoundException;
 import com.senacor.code.fullstack.chat.service.ChatMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -16,8 +15,10 @@ import java.util.List;
  * The REST controller for managing chat messages
  */
 @RestController
-@RequestMapping("/api/v1/{channel}/messages")
+@RequestMapping(ChatMessageController.CHAT_MESSAGES_PATH)
 public class ChatMessageController {
+
+    static final String CHAT_MESSAGES_PATH = "/api/v1/{channel}/messages";
 
     private ChatMessageService messageService;
 
@@ -31,6 +32,19 @@ public class ChatMessageController {
         try {
             List<ChatMessage> chatMessages = messageService.loadChatMessages(channel);
             return ResponseEntity.ok().body(chatMessages);
+        } catch (ChannelNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> newChatMessages(@PathVariable("channel") String channel, @RequestBody ChatMessage chatMessage) {
+        try {
+            ChatMessage newChatMessage = messageService.createChatMessage(channel, chatMessage.getSender(), chatMessage.getMessage());
+
+            UriComponents location = UriComponentsBuilder.newInstance().path(CHAT_MESSAGES_PATH).pathSegment(newChatMessage.getId()).buildAndExpand(channel);
+            return ResponseEntity.created(location.toUri()).build();
+
         } catch (ChannelNotFoundException e) {
             return ResponseEntity.notFound().build();
         }
