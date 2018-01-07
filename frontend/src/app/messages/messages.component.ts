451f3028b63fa200ea7d5afa6171d029eb4495ff
Andri Bremm <andri.bremm@senacor.com>
2018-01-07 23:08:39 +0100

    Task 5.1: Show some simple messages

---
+++ frontend/src/app/messages/messages.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {ChatMessage} from "../shared/chat-message.model";
+
+const MESSAGES : ChatMessage[] = [
+  new ChatMessage("Hello World!"),
+  new ChatMessage("This is another message.")
+];
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent implements OnInit {
+
+  messages: ChatMessage[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.fetchChatMessages().then(messages => this.messages = messages);
+  }
+
+  private fetchChatMessages(): Promise<ChatMessage[]> {
+    return Promise.resolve(MESSAGES.slice(0)); // TODO: get message data from the server;
+  }
+}
