9c9a294f6c7ce06a57c5337609e515085f4bb509
Andri Bremm <andri.bremm@senacor.com>
2018-01-09 08:31:18 +0100

    Task 6.1 A form to select a channel.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 
 import { AppComponent } from './app.component';
 import { MessagesComponent } from './messages/messages.component';
+import { ChannelSelectorComponent } from './channel-selector/channel-selector.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MessagesComponent
+    MessagesComponent,
+    ChannelSelectorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/channel-selector/channel-selector.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import {Channel} from "../shared/channel.model";
+
+const CHANNELS : Channel[] = [
+  new Channel("general"),
+  new Channel("dev"),
+  new Channel("humor")
+];
+
+@Component({
+  selector: 'app-channel-selector',
+  templateUrl: './channel-selector.component.html',
+  styleUrls: ['./channel-selector.component.css']
+})
+export class ChannelSelectorComponent implements OnInit {
+
+  currentChannel: string;
+  channels: Channel[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.fetchChannels().then(channels => {
+      this.channels = channels;
+      this.initCurrentChannel();
+    });
+  }
+
+  public onChannelSelected() {
+    alert(this.currentChannel);
+  }
+
+  private fetchChannels(): Promise<Channel[]> {
+    return Promise.resolve(CHANNELS.slice(0)); // TODO: get channels from the server;
+  }
+
+  private initCurrentChannel() {
+    if (this.currentChannel === undefined && this.channels.length > 0) {
+      this.currentChannel = this.channels[0].name;
+    }
+  }
+
+}
