dabe0d584eae8f6b4a6f704311b5e98d296fd00a
Andri Bremm <andri.bremm@senacor.com>
2018-01-11 08:24:18 +0100

    Task 7.1 Get channels via service call.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 
 import { AppComponent } from './app.component';
@@ -18,6 +19,7 @@ import { MessageFormComponent } from './message-form/message-form.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule
   ],
   providers: [],
